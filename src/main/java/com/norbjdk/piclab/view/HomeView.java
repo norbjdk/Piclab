65561ed511b37b1c5c37eaae9d3463e6fc741f33
norbjdk <norbertd1410@gmail.com>
2026-03-05 19:48:26 +0100

    feat: create new View
    - Add HomeView with no really implementation
    - Empty inheritances

---
+++ src/main/java/com/norbjdk/piclab/view/HomeView.java
@@ -0,0 +1,42 @@
+package com.norbjdk.piclab.view;
+
+import com.norbjdk.piclab.model.ui.Presentable;
+import com.norbjdk.piclab.model.ui.ViewContainer;
+import javafx.scene.control.ScrollPane;
+
+public class HomeView extends ScrollPane implements Presentable, ViewContainer {
+
+    public HomeView() {
+        present();
+    }
+
+    @Override
+    public void initComponents() {
+
+    }
+
+    @Override
+    public void setupComponents() {
+
+    }
+
+    @Override
+    public void setupStyle() {
+
+    }
+
+    @Override
+    public void setupLayout() {
+
+    }
+
+    @Override
+    public void setupEventListeners() {
+
+    }
+
+    @Override
+    public void setupEventHandlers() {
+
+    }
+}
