b84e94ff3991d43d6f44ce784f5e204fe3135329
norbjdk <norbertd1410@gmail.com>
2026-03-05 11:10:29 +0100

    feat: create new MainController
    - Add root field from FXML file
    - Implement methods from Presentable class
    - Implement Initializable interface for controller initialization managing
    - Empty inheritances, everything to do...

---
+++ src/main/java/com/norbjdk/piclab/controller/MainController.java
@@ -0,0 +1,49 @@
+package com.norbjdk.piclab.controller;
+
+import com.norbjdk.piclab.model.ui.Presentable;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController extends Presentable implements Initializable {
+
+    private @FXML BorderPane root;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @Override
+    protected void initComponents() {
+
+    }
+
+    @Override
+    protected void setupComponents() {
+
+    }
+
+    @Override
+    protected void setupStyle() {
+
+    }
+
+    @Override
+    protected void setupLayout() {
+
+    }
+
+    @Override
+    protected void setupEventListeners() {
+
+    }
+
+    @Override
+    protected void setupEventHandlers() {
+
+    }
+}
