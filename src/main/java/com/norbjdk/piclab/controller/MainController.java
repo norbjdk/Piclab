bef077eb156a25d28b319616e49fc0a1237cd800
norbjdk <norbertd1410@gmail.com>
2026-03-05 22:34:34 +0100

    feat: add changing view
    - Add handler for view changed event
    - Add changeView method that rely on ViewResponse

--- src/main/java/com/norbjdk/piclab/controller/MainController.java
+++ src/main/java/com/norbjdk/piclab/controller/MainController.java
@@ -1,9 +1,14 @@
 package com.norbjdk.piclab.controller;
 
 import com.norbjdk.piclab.component.NavigationBar;
+import com.norbjdk.piclab.manager.EventBus;
+import com.norbjdk.piclab.model.dto.ViewResponse;
+import com.norbjdk.piclab.model.event.ViewChangedEvent;
 import com.norbjdk.piclab.model.ui.Presentable;
+import com.norbjdk.piclab.model.ui.ViewContainer;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.layout.BorderPane;
 
 import java.net.URL;
@@ -42,11 +47,29 @@ public class MainController implements Initializable, Presentable {
 
     @Override
     public void setupEventListeners() {
-
+        EventBus.getInstance().subscribe(ViewChangedEvent.class, this::handleViewChanged);
     }
 
     @Override
     public void setupEventHandlers() {
 
+    }
+
+    private <V extends ViewContainer> void changeView(ViewResponse<V> response) {
+        final V newView = response.getView();
+
+        if (newView != null) {
+            final Node currentView = root.getCenter();
+
+            if (currentView != newView) {
+                root.setCenter((Node) newView);
+            }
+        }
+    }
+
+    private <V extends ViewContainer> void handleViewChanged(ViewChangedEvent<V> event){
+        final var response = event.getResponse();
+
+        changeView(response);
     }
 }
